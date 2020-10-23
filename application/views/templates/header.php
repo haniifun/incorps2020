13a3a7ad47d0c5db466fddd3f56f136ea9a7fbd6
hanif <haaniframadhani@gmail.com>
2020-10-23 09:42:30 +0700

    menambahkan fungsi escape

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,22 +3,22 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?= $title ?></title> 
+  <title><?= html_escape($title) ?></title> 
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" sizes="57x57" href="<?= base_url('assets/') ?>img/favicon-incorps.png">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?= base_url('assets/') ?>plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?= html_escape(base_url('assets/')) ?>plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> -->
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="<?= base_url('assets/') ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="<?= esc_url(base_url('assets/')) ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="<?= base_url('assets/') ?>css/adminlte.min.css">
+  <link rel="stylesheet" href="<?= esc_html(base_url('assets/')) ?>css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <!-- <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet"> -->
 
-  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/') ?>css/style.css">
+  <link rel="stylesheet" type="text/css" href="<?= esc_attr(base_url('assets/')) ?>css/style.css">
   <link rel="stylesheet" type="text/css" href="<?= base_url('assets/') ?>css/responsive-style.css">
   <style>
     .text-kecil{
